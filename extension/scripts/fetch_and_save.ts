af6aed3f98c6c90c5d4fe5541779319c3169cc62
dcep93 <dan@quizlet.com>
2021-09-24 00:08:15 -0700

    type

--- extension/scripts/fetch_and_save.ts
+++ extension/scripts/fetch_and_save.ts
@@ -3,3 +3,7 @@ console.log("fetch_and_save");
 fetch("https://google.com")
   .then((r) => r.text())
   .then((t) => console.log(t));
+
+export type NFLStreamType = { updated: string; streams?: StreamType[] };
+
+export type StreamType = { url: string; name: string };

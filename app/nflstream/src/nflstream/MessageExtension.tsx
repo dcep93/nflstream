80e6dd16eccae494671a52df056a8a31e00a1de6
dcep93 <dan@quizlet.com>
2021-09-24 18:11:12 -0700

    comment

--- app/nflstream/src/nflstream/MessageExtension.tsx
+++ app/nflstream/src/nflstream/MessageExtension.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import firebase, { NFLStreamType } from "../firebase";
+import { NFLStreamType } from "../firebase";
 
 function MessageExtension() {
   const ref: React.RefObject<HTMLTextAreaElement> = React.createRef();
@@ -11,8 +11,7 @@ function MessageExtension() {
       onClick={() => {
         const nflStream: NFLStreamType = JSON.parse(ref.current!.value);
         console.log("skipping update", nflStream);
-        return;
-        firebase.updateNFLStream(nflStream);
+        // firebase.updateNFLStream(nflStream);
       }}
     />
   );

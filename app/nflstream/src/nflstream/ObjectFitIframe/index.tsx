1c8da96a00a90773547b2669a5ba7faafdc7d65e
dcep93 <dan@quizlet.com>
2021-09-27 15:52:15 -0700

    load

--- app/nflstream/src/nflstream/ObjectFitIframe/index.tsx
+++ app/nflstream/src/nflstream/ObjectFitIframe/index.tsx
@@ -3,7 +3,7 @@ import { default as ofStyle } from "./index.module.css";
 function ObjectFitIframe(props: { url: string; title: string }) {
   return (
     <iframe
-      className={ofStyle.sub_screen}
+      className={ofStyle.fill}
       title={props.title}
       srcDoc={`
 <style>
@@ -22,25 +22,39 @@ iframe {
 }
 </style>
 <script>
-function load() {
-    console.log("loading");
-    const iframe = document.getElementById("iframe");
-    const ratioStr = iframe.offsetWidth + "/" + iframe.offsetHeight;
-    const ratio = iframe.offsetWidth / iframe.offsetHeight;
-    const style = document.createElement("style");
-    style.innerHTML = "\
-    iframe { opacity: 1; }\
-    @media (min-aspect-ratio: " + ratioStr + ") { iframe { height: 100vh; width: " + 100 * ratio + "vh; } }\
-    @media (max-aspect-ratio: " + ratioStr + ") { iframe { width: 100vw; height: " + 100 / ratio + "vw; } }\
-";
-    console.log(style.innerHTML);
-    document.head.appendChild(style);
-}
+${load}
 </script>
 <iframe id="iframe" src="${props.url}" onload="load()" />
-`}
+      `}
     ></iframe>
   );
+}
+
+function load() {
+  console.log("loading");
+  const iframe = document.getElementById("iframe")!;
+  const ratioStr = iframe.offsetWidth + "/" + iframe.offsetHeight;
+  const ratio = iframe.offsetWidth / iframe.offsetHeight;
+  const style = document.createElement("style");
+  style.innerHTML = `
+      iframe {
+          opacity: 1;
+      }
+      @media (min-aspect-ratio: ${ratioStr}) {
+          iframe {
+              height: 100vh;
+              width: ${100 * ratio}vh;
+          }
+      }
+      @media (max-aspect-ratio: ${ratioStr}) {
+          iframe {
+              width: 100vw;
+              height: ${100 / ratio}vw;
+          }
+      }
+  `;
+  console.log(style.innerHTML);
+  document.head.appendChild(style);
 }
 
 export default ObjectFitIframe;

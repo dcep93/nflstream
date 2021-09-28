57e98494288eb8321536f4dfb7b8c66cd3247f92
dcep93 <dan@quizlet.com>
2021-09-28 14:11:41 -0700

    simpler

--- extension/nflstream/background.js
+++ extension/nflstream/background.js
@@ -1,52 +1,6 @@
-console.log("background", new Date());
-
-var count = 0;
-
-function main(sendMessage) {
-  console.log("main");
-
-  // export type NFLStreamType = { timestamp: number; streams?: StreamType[] };
-
-  // export type StreamType = { url: string; name: string };
-
-  Promise.resolve()
-    .then(() => getRawStreams())
-    .then((rawStreams) => rawStreams.map(getStream))
-    .then((streamPromises) => Promise.all(streamPromises))
-    .then((streams) =>
-      sendMessage({
-        timestamp: new Date().getTime(),
-        streams,
-      })
-    )
-    .then(() => console.log("done", ++count));
-}
-
-function getRawStreams() {
-  return [];
-}
-
-function getStream(rawStream) {
-  return rawStream;
-}
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  main(sendResponse);
-  return true;
-});
-
-chrome.browserAction.onClicked.addListener((tab) => {
-  main((message) =>
-    fetch("https://nflstream.web.app")
-      .then((resp) => resp.text())
-      .then((text) =>
-        chrome.tabs.create(
-          { url: chrome.runtime.getURL("nflstream.html") },
-          function (tab_) {
-            // chrome.tabs.sendMessage(tab.id, message);
-            // setTimeout(() => chrome.tabs.sendMessage(tab_.id, text), 100);
-          }
-        )
-      )
-  );
-});
+chrome.browserAction.onClicked.addListener((tab) =>
+  chrome.tabs.create(
+    { url: chrome.runtime.getURL("nflstream.html") },
+    function (tab_) {}
+  )
+);

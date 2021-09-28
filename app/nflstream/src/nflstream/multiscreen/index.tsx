4f56ad26055a6fc92b41f1d2a7c3a6227f11169b
dcep93 <dan@quizlet.com>
2021-09-27 20:41:34 -0700

    yeah

--- app/nflstream/src/nflstream/multiscreen/index.tsx
+++ app/nflstream/src/nflstream/multiscreen/index.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { StreamType } from "../../firebase";
-import { default as style } from "../index.module.css";
+import style from "../index.module.css";
 import ObjectFitIframe from "../ObjectFitIframe";
-import { default as msStyle } from "./index.module.css";
+import msStyle from "./index.module.css";
 
 export type ScreenType = StreamType & { iFrameTitle: string };
 
@@ -10,64 +10,48 @@ function Multiscreen(props: {
   screens: ScreenType[];
   removeScreen: (index: number) => void;
 }) {
-  return (
-    <MultiscreenStateful
-      screens={props.screens.map((s, i) => ({
-        e: (
-          <Singlescreen
-            key={s.iFrameTitle}
-            screen={s}
-            delete={() => props.removeScreen(i)}
-          />
-        ),
-        s,
-        i,
-      }))}
-    />
-  );
-}
-
-function MultiscreenStateful(props: {
-  screens: { e: JSX.Element; s: ScreenType; i: number }[];
-}) {
-  const [selected, updateSelected] = useState("");
-  var spotlightIndex =
-    props.screens.find((obj) => obj.s.iFrameTitle === selected)?.i || 0;
+  const [rawSelected, updateSelected] = useState("");
+  const selected = rawSelected || props.screens[0]?.iFrameTitle;
   return (
     <div className={msStyle.screens_wrapper}>
       {props.screens.length === 0 ? null : (
         <div className={msStyle.screens}>
-          <div className={msStyle.spotlight}>
-            {props.screens[spotlightIndex]!.e}
-          </div>
-          {props.screens.length <= 1 ? null : (
-            <div className={msStyle.aux_screens}>
-              {props.screens
-                .map((w, i) => ({ w, i }))
-                .filter((_, i) => i !== spotlightIndex)
-                .map((obj) => (
-                  <div
-                    className={msStyle.aux_screen_wrapper}
-                    key={obj.w.s.iFrameTitle}
-                  >
-                    <div
-                      className={msStyle.aux_screen_cover}
-                      onClick={() => updateSelected(obj.w.s.iFrameTitle)}
-                    ></div>
-                    {obj.w.e}
-                  </div>
-                ))}
-            </div>
-          )}
+          {props.screens.map((screen, i) => (
+            <Singlescreen
+              key={screen.iFrameTitle}
+              screen={screen}
+              numScreens={props.screens.length}
+              selected={selected}
+              updateSelected={updateSelected}
+              delete={() => props.removeScreen(i)}
+            />
+          ))}
         </div>
       )}
     </div>
   );
 }
 
-function Singlescreen(props: { delete: () => void; screen: ScreenType }) {
+function Singlescreen(props: {
+  screen: ScreenType;
+  numScreens: number;
+  selected: string;
+  updateSelected: (selected: string) => void;
+  delete: () => void;
+}) {
   return (
-    <div className={msStyle.screen_wrapper}>
+    <div
+      style={{
+        width:
+          props.screen.iFrameTitle === props.selected
+            ? undefined
+            : `${100 / props.numScreens}%`,
+      }}
+      className={[
+        props.screen.iFrameTitle === props.selected && msStyle.selected_screen,
+        msStyle.screen_wrapper,
+      ].join(" ")}
+    >
       <div
         className={[msStyle.title, style.hover].join(" ")}
         onClick={props.delete}
@@ -75,6 +59,11 @@ function Singlescreen(props: { delete: () => void; screen: ScreenType }) {
         {props.screen.name}
       </div>
       <div className={msStyle.screen}>
+        <div
+          hidden={props.screen.iFrameTitle === props.selected}
+          className={msStyle.screen_mask}
+          onClick={() => props.updateSelected(props.screen.iFrameTitle)}
+        ></div>
         <ObjectFitIframe
           url={props.screen.url}
           title={`${props.screen.name}\n${props.screen.iFrameTitle}`}

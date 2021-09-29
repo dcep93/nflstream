eec764764a7ee075104717a571f3b2f40978d7e1
dcep93 <dan@quizlet.com>
2021-09-28 17:38:30 -0700

    ext

--- extension/nflstream/background.js
+++ extension/nflstream/background.js
@@ -3,15 +3,11 @@ function main() {
   return Promise.resolve();
 }
 
-const url = "https://nflstream.web.app";
-chrome.browserAction.onClicked.addListener((tab) =>
-  main()
-    .then(() => tab.getURL())
-    .then(
-      (tabUrl) =>
-        tabUrl.startswith(url) ||
-        chrome.tabs.create({ url }, function (tab_) {})
-    )
-);
+const url = "https://nflstream.web.app/";
+chrome.browserAction.onClicked.addListener((tab) => {
+  main().then(
+    () => tab.url === url || chrome.tabs.create({ url }, function (tab_) {})
+  );
+});
 
 chrome.runtime.onMessage.addListener(main);

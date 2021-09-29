796a5a47c40ef19272fdaa38322eedc1c73b03c4
dcep93 <dan@quizlet.com>
2021-09-28 17:32:06 -0700

    extension dummy

--- extension/nflstream/background.js
+++ extension/nflstream/background.js
@@ -1,6 +1,17 @@
+function main() {
+  console.log("main");
+  return Promise.resolve();
+}
+
+const url = "https://nflstream.web.app";
 chrome.browserAction.onClicked.addListener((tab) =>
-  chrome.tabs.create(
-    { url: chrome.runtime.getURL("nflstream.html") },
-    function (tab_) {}
-  )
+  main()
+    .then(() => tab.getURL())
+    .then(
+      (tabUrl) =>
+        tabUrl.startswith(url) ||
+        chrome.tabs.create({ url }, function (tab_) {})
+    )
 );
+
+chrome.runtime.onMessage.addListener(main);
--- extension/nflstream/nflstream.js
+++
@@ -1,7 +0,0 @@
-const url = "https://nflstream.web.app";
-fetch(url)
-  .then((resp) => resp.text())
-  .then((text) => text.replaceAll(/\/?static/g, `${url}/static`))
-  .then((text) => {
-    document.getElementById("iframe").srcdoc = text;
-  });

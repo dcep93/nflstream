cead3b9e1fd62b2ad6a6829deffff239b2d81dce
dcep93 <dan@quizlet.com>
2021-09-28 17:39:46 -0700

    no double

--- extension/nflstream/background.js
+++ extension/nflstream/background.js
@@ -4,10 +4,8 @@ function main() {
 }
 
 const url = "https://nflstream.web.app/";
-chrome.browserAction.onClicked.addListener((tab) => {
-  main().then(
-    () => tab.url === url || chrome.tabs.create({ url }, function (tab_) {})
-  );
-});
+chrome.browserAction.onClicked.addListener((tab) =>
+  tab.url === url ? main() : chrome.tabs.create({ url }, function (tab_) {})
+);
 
 chrome.runtime.onMessage.addListener(main);

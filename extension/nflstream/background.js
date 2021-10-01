32d8cd03dd1998fa4419127d714cbf29d5c17db6
dcep93 <dan@quizlet.com>
2021-09-30 21:53:53 -0700

    ext

--- extension/nflstream/background.js
+++ extension/nflstream/background.js
@@ -1,16 +1,43 @@
-function main(src) {
-  console.log("main", src);
-  return Promise.resolve({});
+function sendMessage(tabId, payload) {
+  return new Promise((resolve) => {
+    chrome.tabs.sendMessage(tabId, payload, resolve);
+  });
 }
 
 const url = "https://nflstream.web.app/";
 chrome.action.onClicked.addListener((tab) =>
   tab.url === url
-    ? main("click").then((msg) => chrome.tabs.sendMessage(tab.id, msg))
+    ? main("click", tab.id).then((message) =>
+        sendMessage(tab.id, { type: "main", message })
+      )
     : chrome.tabs.create({ url }, function (tab_) {})
 );
 
 chrome.runtime.onMessage.addListener(
   (message, sender, sendResponse) =>
-    main("visit").then((msg) => sendResponse(msg)) && true
+    main("visit", sender.tab.id).then((message) =>
+      sendResponse({ type: "main", message })
+    ) && true
 );
+
+function main(src, tabId) {
+  console.log("main", src);
+  return fetch("https://reddit.nflbite.com/")
+    .then((resp) => resp.text())
+    .then((message) => sendMessage(tabId, { type: "parseGames", message }))
+    .then((hrefs) =>
+      hrefs.map((href) =>
+        fetch(href)
+          .then((resp) => resp.text())
+          .then((message) =>
+            sendMessage(tabId, { type: "parseLinks", message })
+          )
+      )
+    )
+    .then((promises) => Promise.all(promises));
+}
+
+function log(arg) {
+  console.log(arg);
+  return arg;
+}
--- extension/nflstream/content_script.js
+++ extension/nflstream/content_script.js
@@ -1,12 +1,57 @@
 console.log("content_script");
 
+function receive(payload, sender, sendResponse) {
+  console.log("receive", payload);
+  Promise.resolve(payload.message)
+    .then({ main, parseGames, parseLinks }[payload.type])
+    .then(sendResponse);
+}
+
 function main(message) {
-  console.log("main", message);
   const div = document.getElementById("message_extension");
   div.value = JSON.stringify(message);
   div.click();
 }
 
-chrome.runtime.onMessage.addListener(main);
+function parse(text) {
+  return Promise.resolve(text).then((text) => {
+    const html = document.createElement("html");
+    html.innerHTML = text;
+    return html;
+  });
+}
 
-chrome.runtime.sendMessage(null, main);
+function parseGames(message) {
+  return parse(message)
+    .then((html) =>
+      Array.from(html.getElementsByClassName("competition")).find(
+        (competition) =>
+          Array.from(competition.getElementsByClassName("name")).find(
+            (name) => name.innerHTML === "NFL, Regular Season"
+          )
+      )
+    )
+    .then((competition) =>
+      Array.from(competition.getElementsByClassName("col-md-6"))
+        .filter(
+          (match) =>
+            match.getElementsByClassName("status")[0].innerHTML === "finished"
+        )
+        .map((match) => match.getElementsByTagName("a")[0].href)
+    );
+}
+
+function parseLinks(message) {
+  return Promise.resolve(message)
+    .then(parse)
+    .then((html) => html.innerHTML);
+}
+
+chrome.runtime.onMessage.addListener(receive);
+
+chrome.runtime.sendMessage(null, receive);
+
+function log(arg) {
+  console.log(arg);
+  return arg;
+}

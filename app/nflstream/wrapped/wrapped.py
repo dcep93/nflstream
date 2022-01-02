743b6322927e6585cad866051a757953bc878cf8
dcep93 <dan@quizlet.com>
2022-01-02 14:16:26 -0800

    fetch

--- app/nflstream/wrapped/wrapped.py
+++ app/nflstream/wrapped/wrapped.py
@@ -1,6 +1,19 @@
 import concurrent.futures
 
+import json
+import requests
+
+league_id = 203836968
+
 metrics = []
+
+cache_path = "cache.json"
+
+with open(cache_path) as fh:
+    try:
+        cache = json.load(fh)
+    except:
+        cache = {}
 
 
 def main():
@@ -16,8 +29,12 @@ def main():
         number = metric[0][2]
         if number > 0:
             points = points[:number]
-        for point in points:
-            print(point)
+        if points:
+            for point in points:
+                print(point)
+        else:
+            print("no points")
+        print()
 
 
 def metric_d(name, number=0):
@@ -28,12 +45,31 @@ def metric_d(name, number=0):
     return d
 
 
+def fetch(url):
+    if url in cache:
+        return cache[url]
+    print(f"fetching {url}")
+    data = requests.get(url).json()
+    cache[url] = data
+    with open(cache_path, "w") as fh:
+        json.dump(cache, fh)
+    return data
+
+
 ###
+
+weeks = range(1, 14)
 
 
 @metric_d("times chosen wrong")
 def times_chosen_wrong():
-    return [1, 2, 3]
+    points = []
+    for week in weeks:
+        url = f"https://fantasy.espn.com/apis/v3/games/ffl/seasons/2021/segments/0/leagues/{league_id}?view=mScoreboard&scoringPeriodId={week}"
+        data = fetch(url)
+        print(data)
+        break
+    return points
 
 
 main()

3be3a37603fae17f241cbe183f4eb3f9015b386a
dcep93 <dan@quizlet.com>
2022-01-04 11:56:39 -0800

    x

--- app/nflstream/src/wrapped/index.tsx
+++ app/nflstream/src/wrapped/index.tsx
@@ -7,12 +7,12 @@ type WrappedType = {
     number: number;
     matches: TeamType[][];
     boxscores: {
-      name: string;
+      oppTeam: ProTeam;
       passing: number;
       rushing: number;
       score: number;
     }[];
-    playbyplays: string[];
+    playbyplays: { team: ProTeam; headlines: string[] }[];
   }[];
 };
 
@@ -23,7 +23,58 @@ type TeamType = {
   roster: { [playerId: string]: PlayerType };
 };
 
-type PlayerType = { name: string; score: number };
+type PlayerType = {
+  name: string;
+  score: number;
+  position: Position;
+  team: ProTeam;
+};
+
+enum Position {
+  QB = 1,
+  RB = 2,
+  WR = 3,
+  TE = 4,
+  K = 5,
+  DST = 16,
+  FLEX = -1,
+}
+
+enum ProTeam {
+  FA = 0,
+  ATL = 1,
+  BUF = 2,
+  CHI = 3,
+  CIN = 4,
+  CLE = 5,
+  DAL = 6,
+  DEN = 7,
+  DET = 8,
+  GB = 9,
+  TEN = 10,
+  IND = 11,
+  KC = 12,
+  LV = 13,
+  LAR = 14,
+  MIA = 15,
+  MIN = 16,
+  NE = 17,
+  NO = 18,
+  NYG = 19,
+  NYJ = 20,
+  PHI = 21,
+  ARI = 22,
+  PIT = 23,
+  LAC = 24,
+  SF = 25,
+  SEA = 26,
+  TB = 27,
+  WSH = 28,
+  CAR = 29,
+  JAX = 30,
+  BAL = 31,
+  HOU = 32,
+}
 
 const leagueId = 203836968;
 

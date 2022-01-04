e2ada817c545bf08de6e2b60b5b62ba01e651bb2
dcep93 <dan@quizlet.com>
2022-01-04 14:30:54 -0800

    x

--- app/nflstream/src/wrapped/index.tsx
+++ app/nflstream/src/wrapped/index.tsx
@@ -1,33 +1,44 @@
 import React from "react";
 import all_data from "./wrapped.json";
 
+const leagueId = 203836968;
+
+function Wrapped() {
+  const data: WrappedType = all_data[leagueId];
+  if (!data) return <div>no data found for league {leagueId}</div>;
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+}
+
 type WrappedType = {
-  teams: string[];
-  weeks: {
-    number: number;
-    matches: TeamType[][];
-    boxscores: {
-      oppTeam: ProTeam;
-      passing: number;
-      rushing: number;
-      score: number;
-    }[];
-    playbyplays: { team: ProTeam; headlines: string[] }[];
+  teamNames: string[];
+  weeks: WeekType[];
+};
+
+type WeekType = {
+  number: number;
+  matches: TeamType[][];
+  boxscores: {
+    oppTeam: string;
+    passing: number;
+    rushing: number;
+    score: number;
   }[];
+  playbyplays: { team: string; headlines: string[] }[];
 };
 
 type TeamType = {
   teamIndex: number;
   score: number;
-  lineup: string[];
-  roster: { [playerId: string]: PlayerType };
+  lineup: number[];
+  roster: PlayerType[];
 };
 
 type PlayerType = {
+  id: number;
   name: string;
   score: number;
   position: Position;
-  team: ProTeam;
+  team: string;
 };
 
 enum Position {
@@ -38,50 +49,6 @@ enum Position {
   K = 5,
   DST = 16,
   FLEX = -1,
-}
-
-enum ProTeam {
-  FA = 0,
-  ATL = 1,
-  BUF = 2,
-  CHI = 3,
-  CIN = 4,
-  CLE = 5,
-  DAL = 6,
-  DEN = 7,
-  DET = 8,
-  GB = 9,
-  TEN = 10,
-  IND = 11,
-  KC = 12,
-  LV = 13,
-  LAR = 14,
-  MIA = 15,
-  MIN = 16,
-  NE = 17,
-  NO = 18,
-  NYG = 19,
-  NYJ = 20,
-  PHI = 21,
-  ARI = 22,
-  PIT = 23,
-  LAC = 24,
-  SF = 25,
-  SEA = 26,
-  TB = 27,
-  WSH = 28,
-  CAR = 29,
-  JAX = 30,
-  BAL = 31,
-  HOU = 32,
-}
-
-const leagueId = 203836968;
-
-function Wrapped() {
-  const data: WrappedType = all_data[leagueId];
-  if (!data) return `no data found for league ${leagueId}`;
-  return <div>{JSON.stringify(data)}</div>;
 }
 
 export default Wrapped;
---
+++ app/nflstream/src/wrapped/wrapped.py
@@ -0,0 +1,227 @@
+import concurrent.futures
+
+import json
+import requests
+
+from bs4 import BeautifulSoup  # type: ignore
+
+year = 2021
+league_ids = [203836968]
+
+wrapped_path = "wrapped.json"
+
+num_threads = 32
+
+fetch_cache = {}
+
+
+def main():
+    wrapped = {}
+    for league_id in league_ids:
+        wrapped[league_id] = get_wrapped(league_id)
+    with open(wrapped_path, "w") as fh:
+        json.dump(wrapped, fh)
+    print(f"wrote {len(json.dumps(wrapped)) / 1000} kb")
+
+
+def get_wrapped(league_id):
+    weeks_range = range(1, 3)
+    team_names = get_team_names(league_id)
+    weeks = get_weeks(league_id, weeks_range)
+    populate_boxscores(weeks)
+    populate_playbyplays(weeks)
+    return {"teamNames": team_names, "weeks": weeks}
+
+
+def fetch(url, decode_json=True):
+    if url in fetch_cache:
+        return fetch_cache[url]
+    print("fetching", url)
+    raw_data = requests.get(url)
+    data = raw_data.json() if decode_json else raw_data.text
+    fetch_cache[url] = data
+    return data
+
+
+def get_team_names(league_id):
+    data = fetch(
+        f"https://fantasy.espn.com/apis/v3/games/ffl/seasons/{year}/segments/0/leagues/{league_id}?view=mTeam"
+    )
+    return list(
+        map(
+            lambda team: f'{team["location"]} {team["nickname"]}',
+            data["teams"],
+        ), )
+
+
+def get_weeks(league_id, weeks_range):
+    with concurrent.futures.ThreadPoolExecutor(num_threads) as executor:
+        return list(
+            executor.map(
+                lambda week_num: get_matches(league_id, week_num),
+                weeks_range,
+            ))
+
+
+def get_matches(league_id, week_num):
+    url = f"https://fantasy.espn.com/apis/v3/games/ffl/seasons/{year}/segments/0/leagues/{league_id}?view=mScoreboard&scoringPeriodId={week_num}"
+    data = fetch(url)
+    raw_matches = filter(
+        lambda match: "rosterForCurrentScoringPeriod" in match["away"],
+        data["schedule"],
+    )
+    matches = []
+    for match in raw_matches:
+        teams = list(map(
+            get_team,
+            [match["home"], match["away"]],
+        ))
+        matches.append(sorted(
+            teams,
+            key=lambda team: team["score"],
+        ))
+    return {"number": week_num, "matches": matches}
+
+
+def get_team(raw_team):
+    pro_team_names = {
+        0: 'FA',
+        1: 'Atl',
+        2: 'Buf',
+        3: 'Chi',
+        4: 'Cin',
+        5: 'Cle',
+        6: 'Dal',
+        7: 'Den',
+        8: 'Det',
+        9: 'GB',
+        10: 'Ten',
+        11: 'Ind',
+        12: 'KC',
+        13: 'LV',
+        14: 'LAR',
+        15: 'Mia',
+        16: 'Min',
+        17: 'NE',
+        18: 'NO',
+        19: 'NYG',
+        20: 'NYJ',
+        21: 'Phi',
+        22: 'Ari',
+        23: 'Pit',
+        24: 'LAC',
+        25: 'SF',
+        26: 'Sea',
+        27: 'TB',
+        28: 'Wsh',
+        29: 'Car',
+        30: 'Jax',
+        33: 'Bal',
+        34: 'Hou',
+    }
+    lineup = list(
+        map(
+            lambda player: str(player["playerId"]),
+            raw_team["rosterForMatchupPeriod"]["entries"],
+        ))
+    roster = {}
+    for raw_player in raw_team["rosterForCurrentScoringPeriod"]["entries"]:
+        player = raw_player["playerPoolEntry"]["player"]
+        roster[player["id"]] = {
+            "name": player["fullName"],
+            "score":
+            get_points(raw_player["playerPoolEntry"]["appliedStatTotal"]),
+            "position": player["defaultPositionId"],
+            "team": pro_team_names[player['proTeamId']].upper(),
+        }
+    return {
+        "teamIndex": raw_team["teamId"] - 1,
+        "score": get_points(raw_team["totalPoints"]),
+        "lineup": lineup,
+        "roster": roster,
+    }
+
+
+def get_game_id(pro_team_name, week):
+    url = f'https://www.espn.com/nfl/team/schedule/_/name/{pro_team_name}'
+    schedule_html = fetch(url, decode_json=False)
+    soup = BeautifulSoup(schedule_html, features="html.parser")
+    t = soup.find("table")
+    for row in t.findAll("tr"):
+        if row.find("span") and row.find("span").text == str(week):
+            game_url = row.findAll("a", href=True)[2]['href']
+            return game_url.split("espn.com/nfl/game/_/gameId/")[-1]
+
+
+def populate_boxscores(weeks):
+    to_fetch = []
+    for week in weeks:
+        for match in week["matches"]:
+            for team in match:
+                for player in team["roster"].values():
+                    if player["position"] == 16:  # DST
+                        key = (player["team"], week["number"])
+                        to_fetch.append(key)
+
+    with concurrent.futures.ThreadPoolExecutor(num_threads) as executor:
+        fetched_arr = executor.map(lambda f: get_boxscore(*f), to_fetch)
+    fetched_dict = {}
+    for i, fetched in enumerate(fetched_arr):
+        fetched_dict[to_fetch[i]] = fetched
+    for week in weeks:
+        boxscores = []
+        for match in week["matches"]:
+            for team in match:
+                for player in team["roster"].values():
+                    if player["position"] == 16:  # DST
+                        key = (player["team"], week["number"])
+                        boxscores.append(fetched_dict[key])
+        week["boxscores"] = boxscores
+
+
+def get_boxscore(pro_team_name, week):
+    game_id = get_game_id(pro_team_name, week)
+    url = f'https://www.espn.com/nfl/boxscore/_/gameId/{game_id}'
+    box_score_html = fetch(url, decode_json=False)
+    soup = BeautifulSoup(box_score_html, features="html.parser")
+
+    teamabbrevs = list(
+        map(
+            lambda span: span.text,
+            soup.findAll("span", class_="abbrev"),
+        ))
+    key = teamabbrevs.index(pro_team_name)
+
+    all_passing = soup.find(id="gamepackage-passing")
+    table = all_passing.findAll("table")[key]
+    row = table.findAll("tr")[-1]
+    passing_cell = row.findAll("td")[2]
+    passing = int(passing_cell.text)
+
+    all_rushing = soup.find(id="gamepackage-rushing")
+    table = all_rushing.findAll("table")[key]
+    row = table.findAll("tr")[-1]
+    rushing_cell = row.findAll("td")[2]
+    rushing = int(rushing_cell.text)
+
+    score = int(soup.findAll(class_="score")[key].text)
+
+    boxscore = {
+        "team": pro_team_name,
+        "oppTeam": teamabbrevs[1 - key],
+        "passing": passing,
+        "rushing": rushing,
+        "score": score
+    }
+    return boxscore
+
+
+def populate_playbyplays(weeks):
+    pass
+
+
+def get_points(raw_points):
+    return round(raw_points, 2)
+
+
+main()

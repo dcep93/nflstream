8f2e009ae2f804b2b647cecc6f119fc282d9b139
dcep93 <dan@quizlet.com>
2022-01-08 16:34:15 -0800

    x

--- app/nflstream/src/App.tsx
+++ app/nflstream/src/App.tsx
@@ -1,19 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import NFLStream from "./nflstream";
-import Wrapped from "./wrapped";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="wrapped" element={<Wrapped />} />
-        <Route path="*" element={<NFLStream />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <NFLStream />;
+  // return (
+  //   <BrowserRouter>
+  //     <Routes>
+  //       <Route path="wrapped" element={<Wrapped />} />
+  //       <Route path="*" element={<NFLStream />} />
+  //     </Routes>
+  //   </BrowserRouter>
+  // );
 }
 
 export default App;

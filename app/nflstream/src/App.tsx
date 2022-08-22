f42f800633d3a51a9bbf835dc036099bd114a264
dcep93 <dcep93@gmail.com>
2022-08-22 12:36:45 -0700

    x

--- app/nflstream/src/App.tsx
+++ app/nflstream/src/App.tsx
@@ -1,17 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import NFLStream from "./nflstream";
-import Wrapped from "./wrapped";
 
 function App() {
-  return <BrowserRouter>{getApp()}</BrowserRouter>;
-}
-
-function getApp() {
-  // eslint-disable-next-line no-restricted-globals
-  if (location.pathname === "/wrapped") return <Wrapped />;
   return <NFLStream />;
 }
 

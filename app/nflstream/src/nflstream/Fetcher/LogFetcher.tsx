929d8dcb57d124964fecebe470af0a78414e97cf
dcep93 <dcep93@gmail.com>
2022-09-19 17:58:51 -0400

    x

--- app/nflstream/src/nflstream/Fetcher/LogFetcher.tsx
+++ app/nflstream/src/nflstream/Fetcher/LogFetcher.tsx
@@ -3,67 +3,68 @@ import Fetcher, { fetchP, LogType } from ".";
 class LogFetcher extends Fetcher<LogType | null, string> {
   intervalMs = 10 * 1000;
   getResponse() {
-    return fetchP(
-      `https://www.espn.com/nfl/boxscore/_/gameId/${this.props.payload}`,
-      5 * 1000
-    ).then((message) => {
-      const match = message.match(/data: (.*?),\n/);
-      if (!match) return null;
-      const obj = JSON.parse(match[1]).sports[0].leagues[0];
-      console.log(this.props.payload, obj);
-      if (obj.drives === undefined) return null;
-      const drives = [obj.drives.current]
-        .concat(obj.drives.previous.reverse())
-        .filter((drive) => drive.team);
-      const playByPlay = drives.map((drive) => ({
-        team: drive.team.shortDisplayName,
-        result: drive.displayResult,
-        plays: drive.plays.reverse().map((p: any) => ({
-          down: p.start.downDistanceText,
-          text: p.text,
-          clock: `Q${p.period.number} ${p.clock.displayValue}`,
-        })),
-        description: drive.description,
-        score: `${drive.plays[0].awayScore} - ${drive.plays[0].homeScore}`,
-      }));
-      console.log(drives);
-      console.log(
-        drives.flatMap((drive) => drive.plays).map((play) => play.participants)
-      );
-
-      const jerseys = Object.fromEntries(
-        drives
-          .flatMap((drive) => drive.plays)
-          .map((play) => play.participants)
-          .map(({ athlete }) => [athlete.fullName, parseInt(athlete.jersey)])
-      );
-      alert(JSON.stringify(jerseys));
-      const timestamp = obj.drives.current.plays[0].modified;
-      const boxScore = ["passing", "rushing", "receiving"].map((key) => ({
-        key,
-        labels: obj.boxscore.players[0].statistics.find(
-          (s: any) => s.name === key
-        ).labels,
-        players: []
-          .concat(
-            ...obj.boxscore.players
-              .map((team: any) =>
-                team.statistics.find((s: any) => s.name === key)
-              )
-              .map((t: any) => t.athletes)
+    return Promise.resolve()
+      .then(() =>
+        ["boxscore", "playbyplay"]
+          .map(
+            (key) =>
+              `https://www.espn.com/nfl/${key}/_/gameId/${this.props.payload}`
           )
-          .map((a: any) => ({
-            name: a.athlete.displayName,
-            stats: a.stats,
-          }))
-          .map((a) => ({ jersey: jerseys[a.name], ...a })),
-      }));
-      return {
-        timestamp,
-        playByPlay,
-        boxScore,
-      };
-    });
+          .map((url) => fetchP(url, 5 * 1000))
+      )
+      .then((promises) => Promise.all(promises))
+      .then(([boxscoreResp, playbyplayResp]) => {
+        console.log(this.props.payload, boxscoreResp, playbyplayResp);
+        const drives = JSON.parse(playbyplayResp);
+        const boxscore = JSON.parse(boxscoreResp);
+        const drivesArr = [drives.current]
+          .concat(drives.previous.reverse())
+          .filter((drive) => drive.team);
+        const playByPlay = drivesArr.map((drive) => ({
+          team: drive.team.shortDisplayName,
+          result: drive.displayResult,
+          plays: drive.plays.reverse().map((p: any) => ({
+            down: p.start.downDistanceText,
+            text: p.text,
+            clock: `Q${p.period.number} ${p.clock.displayValue}`,
+          })),
+          description: drive.description,
+          score: `${drive.plays[0].awayScore} - ${drive.plays[0].homeScore}`,
+        }));
+        const jerseys = Object.fromEntries(
+          drivesArr
+            .flatMap((drive) => drive.plays)
+            .map((play) => play.participants)
+            .map(({ athlete }) => [athlete.fullName, parseInt(athlete.jersey)])
+        );
+        alert(JSON.stringify(jerseys));
+        return null;
+        const timestamp = drives.current.plays[0].modified;
+        const boxScore = ["passing", "rushing", "receiving"].map((key) => ({
+          key,
+          labels: boxscore.players[0].statistics.find(
+            (s: any) => s.name === key
+          ).labels,
+          players: []
+            .concat(
+              ...boxscore.players
+                .map((team: any) =>
+                  team.statistics.find((s: any) => s.name === key)
+                )
+                .map((t: any) => t.athletes)
+            )
+            .map((a: any) => ({
+              name: a.athlete.displayName,
+              stats: a.stats,
+            }))
+            .map((a) => ({ jersey: jerseys[a.name], ...a })),
+        }));
+        return {
+          timestamp,
+          playByPlay,
+          boxScore,
+        };
+      });
   }
 }
 

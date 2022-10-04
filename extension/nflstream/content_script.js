22e2130ae844e93b05981fd8beae554bcf2c67f5
dcep93 <dcep93@gmail.com>
2022-10-03 20:25:33 -0400

    paddingbottom

--- extension/nflstream/content_script.js
+++ extension/nflstream/content_script.js
@@ -2,6 +2,7 @@ console.log("content_script", "nflstream", location.href);
 
 function init() {
   window.addEventListener("message", muteUnmute);
+  removePaddingBottom();
 }
 
 function muteUnmute(event) {
@@ -9,6 +10,11 @@ function muteUnmute(event) {
   const video = document.getElementsByTagName("video")[0];
   console.log(event.data, "muteUnmute", video);
   if (video) video.muted = event.data.mute;
+}
+
+function removePaddingBottom() {
+  const div = document.getElementsByClassName("embed-responsive")[0];
+  if (div) div.style.paddingBottom = 0;
 }
 
 init();

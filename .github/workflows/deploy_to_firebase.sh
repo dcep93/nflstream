2cbe2455fee380f285294845d09bc40515474b4c
dcep93 <dcep93@gmail.com>
2022-10-06 19:14:22 -0400

    @11.13.0

--- .github/workflows/deploy_to_firebase.sh
+++ .github/workflows/deploy_to_firebase.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 cd app
 export GOOGLE_APPLICATION_CREDENTIALS="gac.json"
 echo "$1" > "$GOOGLE_APPLICATION_CREDENTIALS"
-npm install -g firebase-tools
+npm install -g firebase-tools@11.13.0
 gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
 firebase deploy

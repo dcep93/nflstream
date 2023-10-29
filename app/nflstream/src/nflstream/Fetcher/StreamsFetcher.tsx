d88c7951b70a89da9d950fb07dd7f6f84a2e313f
dcep93 <dcep93@gmail.com>
2023-10-29 18:43:56 -0400

    x

--- app/nflstream/src/nflstream/Fetcher/StreamsFetcher.tsx
+++ app/nflstream/src/nflstream/Fetcher/StreamsFetcher.tsx
@@ -127,7 +127,7 @@ function getTopstreamsUrl(stream_id: string): Promise<string> {
 }
 
 function getStreamUrl(message: string) {
-  return `/topstream_1.3.html?${Object.entries({
+  return `/topstream_2.0.html?${Object.entries({
     key: /var key= '(.*)';/,
     masterkey: /var masterkey= '(.*)'/,
     masterinf: /window.masterinf = (.*);/,

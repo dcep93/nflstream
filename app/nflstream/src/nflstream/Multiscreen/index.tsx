7626f560f42f0d57bb5777cd102956f88aa7f8cf
dcep93 <dcep93@gmail.com>
2023-11-27 21:05:16 -0500

    x

--- app/nflstream/src/nflstream/Multiscreen/index.tsx
+++ app/nflstream/src/nflstream/Multiscreen/index.tsx
@@ -274,7 +274,6 @@ function IframeWrapper(props: { screen: ScreenType; key: number }) {
         >
           <iframe
             ref={props.screen.ref}
-            sandbox={"allow-scripts allow-same-origin allow-presentation"}
             style={{
               height: "100%",
               width: "98%",

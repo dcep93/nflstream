0d815ac365560520c7c3c85b40f126f1732cad8d
dcep93 <dcep93@gmail.com>
2023-11-27 22:57:33 -0500

    x

--- app/nflstream/src/nflstream/Multiscreen/TopstreamIframeContents.tsx
+++ app/nflstream/src/nflstream/Multiscreen/TopstreamIframeContents.tsx
@@ -4,165 +4,15 @@ import { clog } from "..";
 export default function TopstreamIframeContents(params: {
   [key: string]: string;
 }) {
-  function CustomScript(params: { [key: string]: string }) {
-    var key = params.key;
-    if (!key) {
-      alert("invalid params");
-      return;
-    }
-
-    const _flowplayer = Object.assign(window).flowplayer;
-    var _flowapi: any;
-
-    const resources: { [key: string]: any } = {};
-
-    Object.assign(window, {
-      resources,
-      router: null,
-      routermode: 1, //1=resolution ,2=cacheproxy ,3=passthru
-      routercountry: "us",
-      masterinf: JSON.parse(atob(params.masterinf)),
-    });
-
-    setInterval(function () {
-      var records = window.performance.getEntriesByType("resource");
-      for (var i = 0; i < records.length; i++) {
-        var record: any = records[i];
-        resources[record.name] = record.responseEnd;
-      }
-      window.performance.clearResourceTimings();
-    }, 200);
-
-    window.onload = function () {
-      // var reloadCount = 0;
-      // var reloadStart = true;
-      // var isplaying = false;
-
-      // var cloudindex = 0;
-      // var edges = {};
-
-      var masterkey = params.masterkey;
-      //var hlsurl='https://tstreams.info/1/'+key;
-      //var hlsurl='https://proxy.tstreams.info/master/nfl/us/'+masterkey;
-      var hlsurl = "https://tstreams.info/" + masterkey + ".m3u8";
-
-      // var timer;
-      _flowplayer.conf = {
-        fullscreen: true,
-        // iOS allows only native fullscreen from within iframes
-        native_fullscreen: true,
-      };
-      fetch("https://topstreams.info/tv/testurl-" + masterkey + ".txt")
-        .then((resp) => resp.text())
-        .then((text) => {
-          Object.assign(window, { testurl: text });
-        })
-        .then(() => initPlayer(hlsurl));
-    };
-
-    function initPlayer(hlsurl: string) {
-      _flowapi = _flowplayer("#hlsjslive", {
-        splash: true,
-        preload: "auto",
-        autoplay: false,
-        ratio: 9 / 16,
-
-        // stream only available via https:
-        // force loading of Flash HLS via https
-        swfHls: "https://releases.flowplayer.org/7.0.4/flowplayerhls.swf",
-
-        clip: {
-          // enable hlsjs in desktop Safari for manual quality selection
-          // CAVEAT: may cause decoding problems with some streams!
-          hlsjs: {
-            safari: true,
-            recoverNetworkError: true,
-            listeners: ["hlsFragLoaded", "hlsLevelLoading", "hlsLevelLoaded"],
-          },
-          live: true,
-          dvr: true,
-          sources: [{ type: "application/x-mpegurl", src: hlsurl }],
-        },
-      });
-      Object.assign(window, {
-        flowapi: _flowapi,
-      });
-      customInit();
-    }
-
-    function customInit() {
-      Array.from(document.getElementsByTagName("a")).find((a) =>
-        a.href.endsWith("/hello/?from=player")
-      )!.style.opacity = "0";
-
-      (document.getElementsByClassName("fp-ui")[0] as HTMLElement).click();
-
-      const video = document.getElementsByTagName("video")[0];
-      var topstream_muted = true;
-      function update_muted() {
-        video.muted = topstream_muted;
-      }
-      update_muted();
-
-      window.addEventListener("message", (event) => {
-        if (event.data.source !== "nflstream") return;
-        topstream_muted =
-          event.data.mute !== null ? event.data.mute : !topstream_muted;
-        update_muted();
-      });
-
-      const loadedInterval = setInterval(() => {
-        if (_flowapi.video.buffer > 0) {
-          clearInterval(loadedInterval);
-          update_muted();
-          window.parent.postMessage(
-            {
-              source: "topstream.html",
-              action: "loaded",
-              iFrameTitle: params.iFrameTitle,
-            },
-            "*"
-          );
-
-          video.currentTime = _flowapi.video.buffer - 5;
-
-          const accelerateInterval = setInterval(() => {
-            const behind = _flowapi.video.buffer - video.currentTime;
-            if (!video.paused && behind > 5) {
-              video.playbackRate = behind > 5 ? 3 : 1;
-            } else if (video.paused || _flowapi.video.buffer > 60) {
-              clearInterval(accelerateInterval);
-              video.playbackRate = 1;
-
-              var recentTimestamp = 0;
-              var stalledTime = 0;
-              const refreshInterval = setInterval(() => {
-                if (video.paused) return;
-                const now = Date.now();
-                if (recentTimestamp === video.currentTime) {
-                  if (now - stalledTime >= 5000) {
-                    window.parent.postMessage(
-                      {
-                        source: "topstream.html",
-                        action: "refresh",
-                        iFrameTitle: params.iFrameTitle,
-                      },
-                      "*"
-                    );
-                    clearInterval(refreshInterval);
-                  }
-                } else {
-                  recentTimestamp = video.currentTime;
-                  stalledTime = now;
-                }
-              }, 100);
-            }
-          }, 100);
-        }
-      }, 10);
-    }
+  function FunctionToScript<T>(props: { t: T; f: (t: T) => void }) {
+    return (
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `\n(${props.f.toString()})(${JSON.stringify(props.t)})\n`,
+        }}
+      ></script>
+    );
   }
-
   return clog(
     ReactDomServer.renderToStaticMarkup(
       <html
@@ -182,12 +32,14 @@ export default function TopstreamIframeContents(params: {
         }
         `}
           </style>
-          <script>
-            {`
-        const _console = Object.assign({}, console);
-        console.log = console.time = console.timeEnd = () => null;
-        `}
-          </script>
+          <FunctionToScript
+            t={undefined}
+            f={() => {
+              const _console = Object.assign({}, console);
+              Object.assign(window, { _console });
+              console.log = console.time = console.timeEnd = () => null;
+            }}
+          />
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/flowplayer/7.2.7/skin/skin.css"
@@ -211,16 +63,185 @@ export default function TopstreamIframeContents(params: {
           <script src="https://topstreams.info/js/jquery-input-file-text.js"></script>
 
           <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
-          <script>
-            {CustomScript.toString()};{CustomScript.name}($
-            {JSON.stringify(params)});
-          </script>
+          <FunctionToScript
+            t={params}
+            f={(params: { [key: string]: string }) => {
+              var key = params.key;
+              if (!key) {
+                alert("invalid params");
+                return;
+              }
+
+              const _flowplayer = Object.assign(window).flowplayer;
+              var _flowapi: any;
+
+              const resources: { [key: string]: any } = {};
+
+              Object.assign(window, {
+                resources,
+                router: null,
+                routermode: 1, //1=resolution ,2=cacheproxy ,3=passthru
+                routercountry: "us",
+                masterinf: JSON.parse(atob(params.masterinf)),
+              });
+
+              setInterval(function () {
+                var records = window.performance.getEntriesByType("resource");
+                for (var i = 0; i < records.length; i++) {
+                  var record: any = records[i];
+                  resources[record.name] = record.responseEnd;
+                }
+                window.performance.clearResourceTimings();
+              }, 200);
+
+              window.onload = function () {
+                // var reloadCount = 0;
+                // var reloadStart = true;
+                // var isplaying = false;
+
+                // var cloudindex = 0;
+                // var edges = {};
+
+                var masterkey = params.masterkey;
+                //var hlsurl='https://tstreams.info/1/'+key;
+                //var hlsurl='https://proxy.tstreams.info/master/nfl/us/'+masterkey;
+                var hlsurl = "https://tstreams.info/" + masterkey + ".m3u8";
+
+                // var timer;
+                _flowplayer.conf = {
+                  fullscreen: true,
+                  // iOS allows only native fullscreen from within iframes
+                  native_fullscreen: true,
+                };
+                fetch(
+                  "https://topstreams.info/tv/testurl-" + masterkey + ".txt"
+                )
+                  .then((resp) => resp.text())
+                  .then((text) => {
+                    Object.assign(window, { testurl: text });
+                  })
+                  .then(() => initPlayer(hlsurl));
+              };
+
+              function initPlayer(hlsurl: string) {
+                _flowapi = _flowplayer("#hlsjslive", {
+                  splash: true,
+                  preload: "auto",
+                  autoplay: false,
+                  ratio: 9 / 16,
+
+                  // stream only available via https:
+                  // force loading of Flash HLS via https
+                  swfHls:
+                    "https://releases.flowplayer.org/7.0.4/flowplayerhls.swf",
+
+                  clip: {
+                    // enable hlsjs in desktop Safari for manual quality selection
+                    // CAVEAT: may cause decoding problems with some streams!
+                    hlsjs: {
+                      safari: true,
+                      recoverNetworkError: true,
+                      listeners: [
+                        "hlsFragLoaded",
+                        "hlsLevelLoading",
+                        "hlsLevelLoaded",
+                      ],
+                    },
+                    live: true,
+                    dvr: true,
+                    sources: [{ type: "application/x-mpegurl", src: hlsurl }],
+                  },
+                });
+                Object.assign(window, {
+                  flowapi: _flowapi,
+                });
+                customInit();
+              }
+
+              function customInit() {
+                Array.from(document.getElementsByTagName("a")).find((a) =>
+                  a.href.endsWith("/hello/?from=player")
+                )!.style.opacity = "0";
+
+                (
+                  document.getElementsByClassName("fp-ui")[0] as HTMLElement
+                ).click();
+
+                const video = document.getElementsByTagName("video")[0];
+                var topstream_muted = true;
+                function update_muted() {
+                  video.muted = topstream_muted;
+                }
+                update_muted();
+
+                window.addEventListener("message", (event) => {
+                  if (event.data.source !== "nflstream") return;
+                  topstream_muted =
+                    event.data.mute !== null
+                      ? event.data.mute
+                      : !topstream_muted;
+                  update_muted();
+                });
+
+                const loadedInterval = setInterval(() => {
+                  if (_flowapi.video.buffer > 0) {
+                    clearInterval(loadedInterval);
+                    update_muted();
+                    window.parent.postMessage(
+                      {
+                        source: "topstream.html",
+                        action: "loaded",
+                        iFrameTitle: params.iFrameTitle,
+                      },
+                      "*"
+                    );
+
+                    video.currentTime = _flowapi.video.buffer - 5;
+
+                    const accelerateInterval = setInterval(() => {
+                      const behind = _flowapi.video.buffer - video.currentTime;
+                      if (!video.paused && behind > 5) {
+                        video.playbackRate = behind > 5 ? 3 : 1;
+                      } else if (video.paused || _flowapi.video.buffer > 60) {
+                        clearInterval(accelerateInterval);
+                        video.playbackRate = 1;
+
+                        var recentTimestamp = 0;
+                        var stalledTime = 0;
+                        const refreshInterval = setInterval(() => {
+                          if (video.paused) return;
+                          const now = Date.now();
+                          if (recentTimestamp === video.currentTime) {
+                            if (now - stalledTime >= 5000) {
+                              window.parent.postMessage(
+                                {
+                                  source: "topstream.html",
+                                  action: "refresh",
+                                  iFrameTitle: params.iFrameTitle,
+                                },
+                                "*"
+                              );
+                              clearInterval(refreshInterval);
+                            }
+                          } else {
+                            recentTimestamp = video.currentTime;
+                            stalledTime = now;
+                          }
+                        }, 100);
+                      }
+                    }, 100);
+                  }
+                }, 10);
+              }
+            }}
+          />
         </head>
 
         <body>
           <div
             id="hlsjslive"
-            className="fp-slim"
+            // @ts-ignore
+            class="fp-slim"
             style={{ display: "block", outline: "none" }}
           ></div>
         </body>

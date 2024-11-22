93479aba24a6420848574abdd53e8be08373ce63
dcep93 <dcep93@gmail.com>
2024-11-21 22:15:42 -0500

    x

--- app/nflstream/src/nflstream/Multiscreen/HostSrcDoc.tsx
+++ app/nflstream/src/nflstream/Multiscreen/HostSrcDoc.tsx
@@ -336,7 +336,7 @@ export default function HostSrcDoc(params: { [key: string]: string }) {
               }
 
               const loadedInterval = setInterval(() => {
-                if (_flowapi.video.buffer > 0) {
+                if (_flowapi.video.buffer > 60) {
                   clearInterval(loadedInterval);
                   update_muted();
                   window.parent.postMessage(

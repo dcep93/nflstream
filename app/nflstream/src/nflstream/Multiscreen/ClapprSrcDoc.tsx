f8036cf842c6fb790e709e58a60cc2fdde65de42
Daniel Cepeda <dcep93@gmail.com>
2025-09-06 01:10:20 -0400

    x

--- app/nflstream/src/nflstream/Multiscreen/ClapprSrcDoc.tsx
+++ app/nflstream/src/nflstream/Multiscreen/ClapprSrcDoc.tsx
@@ -121,219 +121,221 @@ export default function ClapprSrcDoc(params: { [key: string]: string }) {
               plugins: [(window as any).HlsjsPlayback], // provided by clappr-hlsjs-playback script above
             });
 
-            // function customInit() {
-            //   const video = document.getElementsByTagName("video")[0];
-            //   var subscreen_muted = true;
-            //   function update_muted() {
-            //     video.muted = subscreen_muted;
-            //   }
-            //   update_muted();
+            player.on((window as any).Clappr.Events.PLAYER_PLAY, customInit);
 
-            //   window.addEventListener("message", (event) => {
-            //     if (event.data.source !== "nflstream") return;
-            //     subscreen_muted =
-            //       event.data.mute !== null ? event.data.mute : !subscreen_muted;
-            //     update_muted();
-            //   });
+            function customInit() {
+              const video = document.getElementsByTagName("video")[0];
+              var subscreen_muted = true;
+              function update_muted() {
+                video.muted = subscreen_muted;
+              }
+              update_muted();
 
-            //   function muteCommercialLoop() {
-            //     if (!muteCommercial) return;
-            //     const muteCommercialLoopPeriodMs = 1000;
-            //     type Data = {
-            //       channels: number[];
-            //       alpha: number;
-            //       diff: number;
-            //     };
-            //     function get_data(): Promise<Data[]> {
-            //       if (subscreen_muted) return Promise.resolve([]);
-            //       if (video.videoWidth === 0) {
-            //         return Promise.resolve([]);
-            //       }
-            //       const canvas = document.getElementById(
-            //         "canvas"
-            //       ) as HTMLCanvasElement;
-            //       if (!canvas) {
-            //         return Promise.resolve([]);
-            //       }
-            //       const ctx = canvas.getContext("2d")!;
-            //       ctx.clearRect(0, 0, video.videoWidth, video.videoHeight);
-            //       ctx.drawImage(
-            //         video,
-            //         0,
-            //         0,
-            //         video.videoWidth,
-            //         video.videoHeight
-            //       );
+              window.addEventListener("message", (event) => {
+                if (event.data.source !== "nflstream") return;
+                subscreen_muted =
+                  event.data.mute !== null ? event.data.mute : !subscreen_muted;
+                update_muted();
+              });
 
-            //       const raw_data = ctx.getImageData(
-            //         0,
-            //         0,
-            //         video.videoWidth,
-            //         video.videoHeight
-            //       ).data;
-            //       const num_channels = 4;
-            //       const num_segments = 40;
-            //       const segment_size =
-            //         raw_data.length / num_channels / num_segments;
-            //       if (segment_size % 1 !== 0) return Promise.resolve([]);
-            //       const segments: Data[][] = [];
-            //       function helper(segment_index: number): Promise<Data[]> {
-            //         if (subscreen_muted) return Promise.resolve([]);
-            //         if (segment_index === num_segments)
-            //           return Promise.resolve(segments.flatMap((s) => s));
-            //         segments.push(
-            //           Array.from(new Array(segment_size))
-            //             .map((_, i) => i + segment_index * segment_size)
-            //             .map((i) =>
-            //               Array.from(
-            //                 raw_data.slice(
-            //                   i * num_channels,
-            //                   (i + 1) * num_channels
-            //                 )
-            //               )
-            //             )
-            //             .map((channels) => ({
-            //               channels: channels.slice(0, 3),
-            //               alpha: channels[3],
-            //             }))
-            //             .map((o) => ({
-            //               channels: o.channels,
-            //               alpha: o.alpha,
-            //               avg:
-            //                 o.channels.reduce((a, b) => a + b, 0) /
-            //                 o.channels.length,
-            //             }))
-            //             .map((o) => ({
-            //               channels: o.channels,
-            //               alpha: o.alpha,
-            //               avg: o.avg,
-            //               diff: o.channels
-            //                 .map((c) => Math.abs(c - o.avg))
-            //                 .reduce((a, b) => a + b, 0),
-            //             }))
-            //         );
-            //         return new Promise((resolve) =>
-            //           setTimeout(
-            //             resolve,
-            //             muteCommercialLoopPeriodMs / (num_segments + 1)
-            //           )
-            //         ).then(() => helper(segment_index + 1));
-            //       }
-            //       return helper(0);
-            //     }
-            //     function get_is_commercial(data: Data[]) {
-            //       const filtered = {
-            //         greys: data.filter((d) => d.alpha === 0 && d.diff <= 5)
-            //           .length,
-            //         whites: data.filter((d) => d.alpha === 255 && d.diff <= 5)
-            //           .length,
-            //         blues: data.filter(
-            //           (d) => d.channels[2] - d.channels[0] - d.channels[1] > 20
-            //         ).length,
-            //       };
-            //       const is_commercial =
-            //         filtered.greys >= 876_600 &&
-            //         filtered.whites + filtered.blues >= 44_000 &&
-            //         filtered.blues >= 20;
-            //       return is_commercial;
-            //     }
-            //     function mute_if_commercial() {
-            //       const start_time = Date.now();
-            //       Promise.resolve()
-            //         .then(() => get_data())
-            //         .catch(() => [])
-            //         .then((sliced_data) => {
-            //           const is_commercial = get_is_commercial(sliced_data);
-            //           const should_mute = subscreen_muted || is_commercial;
-            //           if (should_mute !== video.muted) {
-            //             video.muted = should_mute;
-            //           }
-            //           const mute_duration = Date.now() - start_time;
-            //           // console.log({ mute_duration });
-            //           setTimeout(
-            //             () => mute_if_commercial(),
-            //             Math.max(0, muteCommercialLoopPeriodMs - mute_duration)
-            //           );
-            //         });
-            //     }
-            //     mute_if_commercial();
-            //   }
+              function muteCommercialLoop() {
+                if (!muteCommercial) return;
+                const muteCommercialLoopPeriodMs = 1000;
+                type Data = {
+                  channels: number[];
+                  alpha: number;
+                  diff: number;
+                };
+                function get_data(): Promise<Data[]> {
+                  if (subscreen_muted) return Promise.resolve([]);
+                  if (video.videoWidth === 0) {
+                    return Promise.resolve([]);
+                  }
+                  const canvas = document.getElementById(
+                    "canvas"
+                  ) as HTMLCanvasElement;
+                  if (!canvas) {
+                    return Promise.resolve([]);
+                  }
+                  const ctx = canvas.getContext("2d")!;
+                  ctx.clearRect(0, 0, video.videoWidth, video.videoHeight);
+                  ctx.drawImage(
+                    video,
+                    0,
+                    0,
+                    video.videoWidth,
+                    video.videoHeight
+                  );
 
-            //   function catchUp(firstTime: boolean) {
-            //     var currentTime = 0;
-            //     var triggered = false;
-            //     return new Promise<void>((resolve) => {
-            //       const accelerateInterval = setInterval(() => {
-            //         if (!video.paused) {
-            //           const behind = _flowapi.video.buffer - video.currentTime;
-            //           if (behind > (firstTime ? 5 : 20)) {
-            //             triggered = true;
-            //           } else if (behind < 5) {
-            //             triggered = false;
-            //           }
-            //           video.playbackRate = triggered ? 3 : 1;
-            //         }
-            //         if (firstTime) {
-            //           if (_flowapi.video.buffer < 60) {
-            //             return;
-            //           }
-            //         } else {
-            //           if (currentTime <= video.currentTime) {
-            //             currentTime = video.currentTime;
-            //             return;
-            //           }
-            //         }
-            //         video.playbackRate = 1;
-            //         clearInterval(accelerateInterval);
-            //         resolve();
-            //       }, 100);
-            //     });
-            //   }
+                  const raw_data = ctx.getImageData(
+                    0,
+                    0,
+                    video.videoWidth,
+                    video.videoHeight
+                  ).data;
+                  const num_channels = 4;
+                  const num_segments = 40;
+                  const segment_size =
+                    raw_data.length / num_channels / num_segments;
+                  if (segment_size % 1 !== 0) return Promise.resolve([]);
+                  const segments: Data[][] = [];
+                  function helper(segment_index: number): Promise<Data[]> {
+                    if (subscreen_muted) return Promise.resolve([]);
+                    if (segment_index === num_segments)
+                      return Promise.resolve(segments.flatMap((s) => s));
+                    segments.push(
+                      Array.from(new Array(segment_size))
+                        .map((_, i) => i + segment_index * segment_size)
+                        .map((i) =>
+                          Array.from(
+                            raw_data.slice(
+                              i * num_channels,
+                              (i + 1) * num_channels
+                            )
+                          )
+                        )
+                        .map((channels) => ({
+                          channels: channels.slice(0, 3),
+                          alpha: channels[3],
+                        }))
+                        .map((o) => ({
+                          channels: o.channels,
+                          alpha: o.alpha,
+                          avg:
+                            o.channels.reduce((a, b) => a + b, 0) /
+                            o.channels.length,
+                        }))
+                        .map((o) => ({
+                          channels: o.channels,
+                          alpha: o.alpha,
+                          avg: o.avg,
+                          diff: o.channels
+                            .map((c) => Math.abs(c - o.avg))
+                            .reduce((a, b) => a + b, 0),
+                        }))
+                    );
+                    return new Promise((resolve) =>
+                      setTimeout(
+                        resolve,
+                        muteCommercialLoopPeriodMs / (num_segments + 1)
+                      )
+                    ).then(() => helper(segment_index + 1));
+                  }
+                  return helper(0);
+                }
+                function get_is_commercial(data: Data[]) {
+                  const filtered = {
+                    greys: data.filter((d) => d.alpha === 0 && d.diff <= 5)
+                      .length,
+                    whites: data.filter((d) => d.alpha === 255 && d.diff <= 5)
+                      .length,
+                    blues: data.filter(
+                      (d) => d.channels[2] - d.channels[0] - d.channels[1] > 20
+                    ).length,
+                  };
+                  const is_commercial =
+                    filtered.greys >= 876_600 &&
+                    filtered.whites + filtered.blues >= 44_000 &&
+                    filtered.blues >= 20;
+                  return is_commercial;
+                }
+                function mute_if_commercial() {
+                  const start_time = Date.now();
+                  Promise.resolve()
+                    .then(() => get_data())
+                    .catch(() => [])
+                    .then((sliced_data) => {
+                      const is_commercial = get_is_commercial(sliced_data);
+                      const should_mute = subscreen_muted || is_commercial;
+                      if (should_mute !== video.muted) {
+                        video.muted = should_mute;
+                      }
+                      const mute_duration = Date.now() - start_time;
+                      // console.log({ mute_duration });
+                      setTimeout(
+                        () => mute_if_commercial(),
+                        Math.max(0, muteCommercialLoopPeriodMs - mute_duration)
+                      );
+                    });
+                }
+                mute_if_commercial();
+              }
 
-            //   const loadedInterval = setInterval(() => {
-            //     if (_flowapi.video.buffer > 45) {
-            //       clearInterval(loadedInterval);
-            //       update_muted();
-            //       window.parent.postMessage(
-            //         {
-            //           source: "nflstream.html",
-            //           action: "loaded",
-            //           iFrameTitle: params.iFrameTitle,
-            //         },
-            //         "*"
-            //       );
+              function catchUp(firstTime: boolean) {
+                var currentTime = 0;
+                var triggered = false;
+                return new Promise<void>((resolve) => {
+                  const accelerateInterval = setInterval(() => {
+                    if (!video.paused) {
+                      const behind = _flowapi.video.buffer - video.currentTime;
+                      if (behind > (firstTime ? 5 : 20)) {
+                        triggered = true;
+                      } else if (behind < 5) {
+                        triggered = false;
+                      }
+                      video.playbackRate = triggered ? 3 : 1;
+                    }
+                    if (firstTime) {
+                      if (_flowapi.video.buffer < 60) {
+                        return;
+                      }
+                    } else {
+                      if (currentTime <= video.currentTime) {
+                        currentTime = video.currentTime;
+                        return;
+                      }
+                    }
+                    video.playbackRate = 1;
+                    clearInterval(accelerateInterval);
+                    resolve();
+                  }, 100);
+                });
+              }
 
-            //       muteCommercialLoop();
+              const loadedInterval = setInterval(() => {
+                if (_flowapi.video.buffer > 45) {
+                  clearInterval(loadedInterval);
+                  update_muted();
+                  window.parent.postMessage(
+                    {
+                      source: "nflstream.html",
+                      action: "loaded",
+                      iFrameTitle: params.iFrameTitle,
+                    },
+                    "*"
+                  );
 
-            //       video.currentTime = _flowapi.video.buffer - 5;
-            //       catchUp(true).then(() => {
-            //         catchUp(false);
-            //         var recentTimestamp = 0;
-            //         var stalledTime = 0;
-            //         const refreshInterval = setInterval(() => {
-            //           if (video.paused) return;
-            //           const now = Date.now();
-            //           if (recentTimestamp === video.currentTime) {
-            //             if (now - stalledTime >= 5000) {
-            //               window.parent.postMessage(
-            //                 {
-            //                   source: "nflstream.html",
-            //                   action: "refresh",
-            //                   iFrameTitle: params.iFrameTitle,
-            //                 },
-            //                 "*"
-            //               );
-            //               clearInterval(refreshInterval);
-            //             }
-            //           } else {
-            //             recentTimestamp = video.currentTime;
-            //             stalledTime = now;
-            //           }
-            //         }, 100);
-            //       });
-            //     }
-            //   }, 10);
-            // }
+                  muteCommercialLoop();
+
+                  video.currentTime = _flowapi.video.buffer - 5;
+                  catchUp(true).then(() => {
+                    catchUp(false);
+                    var recentTimestamp = 0;
+                    var stalledTime = 0;
+                    const refreshInterval = setInterval(() => {
+                      if (video.paused) return;
+                      const now = Date.now();
+                      if (recentTimestamp === video.currentTime) {
+                        if (now - stalledTime >= 5000) {
+                          window.parent.postMessage(
+                            {
+                              source: "nflstream.html",
+                              action: "refresh",
+                              iFrameTitle: params.iFrameTitle,
+                            },
+                            "*"
+                          );
+                          clearInterval(refreshInterval);
+                        }
+                      } else {
+                        recentTimestamp = video.currentTime;
+                        stalledTime = now;
+                      }
+                    }, 100);
+                  });
+                }
+              }, 10);
+            }
           }}
         />
       </body>

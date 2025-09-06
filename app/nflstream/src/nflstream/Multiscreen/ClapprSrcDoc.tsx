2ea8b59f32191dac752d8f00b1922e339053d990
Daniel Cepeda <dcep93@gmail.com>
2025-09-06 00:33:12 -0400

    x

--- app/nflstream/src/nflstream/Multiscreen/ClapprSrcDoc.tsx
+++ app/nflstream/src/nflstream/Multiscreen/ClapprSrcDoc.tsx
@@ -2,68 +2,26 @@ import { fetchE } from "../Fetcher/LogFetcher";
 
 export const isClappr = process.env.NODE_ENV === "development";
 
+const maxAgeMs = 10 * 60 * 1000;
 export function getClapprParams(
-  url: string,
+  stream_id: string,
   hardRefresh: boolean
 ): Promise<{ [key: string]: string }> {
-  return fetchE(url, hardRefresh ? 0 : 10 * 60 * 1000).then((text) =>
-    Promise.resolve().then(() =>
-      Object.fromEntries(
-        Object.entries({
-          key: /var key= '(.*)';/,
-          masterkey: /var masterkey= '(.*)'/,
-          masterinf: /window.masterinf = (.*);/,
-        })
-          .map(([k, re]) => ({ k, matched: (text.match(re) || [])[1] }))
-          .map(({ k, matched }) => [
-            k,
-            matched?.startsWith("{") ? btoa(matched) : matched,
-          ])
-      )
+  return fetchE("https://icrackstreams.app/nflstreams/live", maxAgeMs)
+    .then(
+      (text) =>
+        Array.from(text.matchAll(/href="(.*?-live-streaming-.*?)" class/))
+          .map((m) => m[0])
+          .find((m) => m.includes(stream_id))!
     )
-  );
+    .then((raw_url) => fetchE(raw_url, hardRefresh ? 0 : maxAgeMs))
+    .then((text) => ({ text }));
 }
 
 export default function getPayload(url: string): Promise<string | undefined> {
-  if (
-    url.includes("//.mp4") ||
-    url.includes(".space/playlist/36343/red5.boostaether.shop/caxi")
-  )
-    return fetchExtension({ url: url.split("//.mp4")[0] });
+  //   if (
+  //     url.includes("//.mp4") ||
+  //     url.includes(".space/playlist/36343/red5.boostaether.shop/caxi")
+  //   )
   return Promise.resolve(undefined);
-}
-
-const extension_id = "dikaanhdjgmmeajanfokkalonmnpfidm";
-
-declare global {
-  interface Window {
-    chrome: any;
-  }
-}
-
-function extensionHelper(payload: any): Promise<any> {
-  return new Promise((resolve, reject) => {
-    if (!window.chrome?.runtime) {
-      return reject("no chrome runtime");
-    }
-    window.chrome.runtime.sendMessage(
-      extension_id,
-      payload,
-      (response: any) => {
-        if (response === undefined) {
-          // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-          window.chrome.runtime.lastError;
-          return reject("empty response");
-        }
-        resolve(response);
-      }
-    );
-  });
-}
-
-export function fetchExtension(request: {
-  url: string;
-  json?: boolean;
-}): Promise<any> {
-  return extensionHelper({ fetch: request });
 }
